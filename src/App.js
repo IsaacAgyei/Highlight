8f4a5c4e50eb7bda385841626554a2eead7b8c4b
Isaac Agyei <agyeis@hotmail.com>
2020-07-06 10:44:44 -0600

    changed folder name

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Twitter from './components/Twitter'
 import Instagram from './components/Instagram'
-import YoutubeSearchBar from './components/YoutubeComponents/YoutubeSearchBar'
+import YoutubeSearchBar from './components/youtubecomponent/YoutubeSearchBar'
 import {Grid, Paper} from '@material-ui/core'
 import {makeStyles} from '@material-ui/core/styles'
 
